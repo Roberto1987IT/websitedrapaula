eaa1d2f1f50e3ff2071afe400e29f0b8c3da4a95
boris <boris.isac@webtech87.com>
2025-04-11 18:09:11 +0100

    [UPDATE] buttons

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,27 +1,28 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import {initReactI18next} from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+
 // Инициализация i18next
 i18n
-  .use(HttpApi) // Подключение загрузчика для файлов JSON
-  .use(LanguageDetector) // Автоматическое определение языка
-  .use(initReactI18next) // Интеграция с React
-  .init({
-    fallbackLng: "en", // Язык по умолчанию
-    supportedLngs: ["en", "ru"], // Список поддерживаемых языков
-    debug: true, // Для отладки (в продакшене можно отключить)
-    interpolation: {
-      escapeValue: false, // Без экранизации для вставки HTML
-    },
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json", // Путь к JSON-файлам
-    },
-    detection: {
-      order: ["localStorage", "cookie", "navigator", "htmlTag"], // Определение языка
-      caches: ["localStorage"], // Сохранение выбора языка
-    },
-  });
+    .use(HttpApi)
+    .use(LanguageDetector)
+    .use(initReactI18next) // Интеграция с React
+    .init({
+        fallbackLng: "pt",
+        supportedLngs: ["en", "pt"],
+        debug: true,
+        interpolation: {
+            escapeValue: false, // Без экранизации для вставки HTML
+        },
+        backend: {
+            loadPath: "/locales/{{lng}}/translation.json", // Путь к JSON-файлам
+        },
+        detection: {
+            order: ["localStorage", "cookie", "navigator", "htmlTag"], // Определение языка
+            caches: ["localStorage"], // Сохранение выбора языка
+        },
+    });
 
 export default i18n;
